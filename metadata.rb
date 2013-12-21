e295cf4558bf909f66c982226d8c49377eb0b933
Bruno Azisaka Maciel <bruno@azisaka.com.br>
2013-12-21 16:35:03 -0200

    Added metadata and README

---
+++ metadata.rb
@@ -0,0 +1,9 @@
+name             'vim'
+maintainer       'Bubble'
+maintainer_email 'contact@bubble.com.br'
+license          'MIT'
+description      'Installs/Configures vim'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.0.1'
+
+depends          'apt'
